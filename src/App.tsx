f3c6d483dbe844d9960ce27f7ed426c77409ae93
Promo656 <rank2007@mail.ru>
2020-07-01 17:35:42 +0300

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {TodoList} from "./TodoList";
 
-function App() {
-    const tasks1=[
-        {id:1,title:'HTML',isDone:true},
-        {id:2,title:'CSS',isDone:false},
-        {id:3,title:'JS',isDone:true},
-        {id:4,title:'TSX',isDone:true}
-    ]
-    const tasks2=[
-        {id:1, title:'Beer',isDone:false},
-        {id:2, title:'Cheeps',isDone:true},
-        {id:3, title:'Pizza',isDone:false},
-        {id:4, title:'Cola',isDone:true}
-    ]
+export type TodoListPropsType = {
+    title: string
+    tasks: Array<TaskType>
+    removeTask:(taskId:number)=> void
+    changeFilter: (newFilterValue:FilterValuesType)=>void
+}
+export type TaskType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+export type FilterValuesType = "all" | "active" | "complited"
+
+export function App() {
+    let [tasks, setTasks] = useState([
+        {id: 1, title: 'HTML', isDone: false},
+        {id: 2, title: 'CSS', isDone: false},
+        {id: 3, title: 'JS', isDone: true},
+        {id: 4, title: 'TSX', isDone: true},
+        {id: 5, title: 'React', isDone: true},
+        {id: 6, title: 'Sass', isDone: false},
+        {id: 7, title: 'LEss', isDone: true}
+    ])
+
+    let [filter, setFilter] = useState<FilterValuesType>("complited")
+
+    function removeTask(taskId: number) {
+        let filteredTasks = tasks.filter((t: TaskType) => t.id !== taskId)
+        setTasks(filteredTasks)
+    }
+
+    function changeFilter(newFilterValue: FilterValuesType) {
+        setFilter(newFilterValue)
+    }
+
+    let taskForTodoList = tasks
+    if (filter === "active") {
+        taskForTodoList = tasks.filter(t => t.isDone === false)
+    }
+
+    if (filter === "complited") {
+        taskForTodoList = tasks.filter(t => t.isDone === true)
+    }
     return (
         <div className="App">
-            <TodoList title='What to learn?' tasks={tasks1}/>
-            <TodoList title='What to buy?' tasks={tasks2}/>
+            <TodoList
+                title='What to learn?'
+                tasks={taskForTodoList}
+                removeTask={removeTask}
+                changeFilter={changeFilter}
+            />
         </div>
     );
 }
-export default App;
+
+
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
-import "./nativeJs/lesson1"
+import {App} from "./App";
+
 
 ReactDOM.render(<App />,  document.getElementById('root'));
 

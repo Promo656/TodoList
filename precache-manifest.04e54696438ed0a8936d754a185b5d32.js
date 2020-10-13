9bc2f81f76da56bffbea66ef6cc748cf461ceae3
Egor <rank2007@mail.ru>
2020-10-13 18:25:14 +0300

    Updates

---
+++ precache-manifest.04e54696438ed0a8936d754a185b5d32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e3ae5edc69210f9675f0f300890b3f3",
+    "url": "/TodoList/index.html"
+  },
+  {
+    "revision": "66b21c3d1c2a780bda58",
+    "url": "/TodoList/static/css/main.7805240e.chunk.css"
+  },
+  {
+    "revision": "19817d56e731cb08e521",
+    "url": "/TodoList/static/js/2.73064019.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/TodoList/static/js/2.73064019.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66b21c3d1c2a780bda58",
+    "url": "/TodoList/static/js/main.05132d8e.chunk.js"
+  },
+  {
+    "revision": "6f4002f704c746794b97",
+    "url": "/TodoList/static/js/runtime-main.b918c17e.js"
+  }
+]);
